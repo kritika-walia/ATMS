371686f7e27e206a3cfe7e836b52037c84d8eeb9
kritika-walia <97731456+kritika-walia@users.noreply.github.com>
2026-04-29 17:02:45 +0530

    Update sync.js

--- api/sync.js
+++ api/sync.js
@@ -1,42 +1,30 @@
 module.exports = async function handler(req, res) {
   try {
 
-    // STEP 1: AUTH API
-    const authResp = await fetch(
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+    const response = await fetch(
       "https://datalakeg.nhai.gov.in/nhaiapi/api/MastersAPI/user_auth?username=Guest_API&password=Guest%402020",
       {
         method: "GET",
         headers: {
-          "api-key": "0f086a6346192a5c68bbf45ac5cd7766",
-          "Content-Type": "application/json"
-        }
-      }
-    );
-
-    const token = await authResp.text();
-
-    // STEP 2: DATA API
-    const dataResp = await fetch(
-      "https://datalakeg.nhai.gov.in/nhaiapi/api/MastersAPI/NHAI_BasicData_Details",
-      {
-        method: "GET",
-        headers: {
-          "Authorization": `Bearer ${token}`,
           "api-key": "0f086a6346192a5c68bbf45ac5cd7766"
         }
       }
     );
 
-    const data = await dataResp.json();
+    const data = await response.text();
 
     return res.status(200).json({
-      message: "API working + data fetched ✅",
-      sample: data[0]
+      message: "Auth working",
+      data: data
     });
 
   } catch (error) {
     return res.status(500).json({
-      error: error.message
+      error: error.message,
+      stack: error.stack,
+      cause: error.cause
     });
   }
 };
