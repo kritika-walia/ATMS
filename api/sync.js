479c717d44ca55b88a3ec51e5d3f09268ac0120f
kritika-walia <97731456+kritika-walia@users.noreply.github.com>
2026-04-29 17:04:25 +0530

    Replace user authentication with Google reachability check

--- api/sync.js
+++ api/sync.js
@@ -1,30 +1,15 @@
 module.exports = async function handler(req, res) {
   try {
 
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
-    const response = await fetch(
-      "https://datalakeg.nhai.gov.in/nhaiapi/api/MastersAPI/user_auth?username=Guest_API&password=Guest%402020",
-      {
-        method: "GET",
-        headers: {
-          "api-key": "0f086a6346192a5c68bbf45ac5cd7766"
-        }
-      }
-    );
-
-    const data = await response.text();
+    const response = await fetch("https://www.google.com");
 
     return res.status(200).json({
-      message: "Auth working",
-      data: data
+      message: "Google reachable ✔"
     });
 
   } catch (error) {
     return res.status(500).json({
-      error: error.message,
-      stack: error.stack,
-      cause: error.cause
+      error: error.message
     });
   }
 };
